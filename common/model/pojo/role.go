a65ad642d498c26622a68d3cd034f7e6eee1c622
zhou-c-y-go <2553598443@qq.com>
2026-05-25 19:38:03 +0800

    修改role.go相关逻辑

--- common/model/pojo/role.go
+++ common/model/pojo/role.go
@@ -12,10 +12,11 @@ type User struct {
 	Email       string    `form:"email" json:"email" gorm:"column:email"`
 	Telephone   string    `form:"telephone" json:"telephone" gorm:"column:telephone"`
 	UUID        uuid.UUID `json:"uuid" gorm:"column:uuid"`
-	Status      int64     `json:"status" gorm:"column:status" form:"status"`
+	Status      int64     `json:"status" gorm:"column:status" form:"status"` // 用户是否被冻结
 	HeadImg     string    `json:"headImg" form:"head-img"`
-	AuthorityId uint      `json:"authorityId" gorm:"default:888;comment:用户角色ID"` // 用户角色ID
+	AuthorityId uint      `json:"authorityId" gorm:"default:888;comment:用户角色ID"` // 角色ID
 	Authority   string    `json:"authority" gorm:"default:user;comment:用户角色"`
+	GetLike     int       `json:"get_like" gorm:"default:0;comment:收到的点赞数"`
 }
 
 type Admin struct {
