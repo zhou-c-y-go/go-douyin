4bb981ccb02f515f15de5f99be51ab2bcec2ec1b
zhou-c-y-go <2553598443@qq.com>
2026-05-25 19:54:17 +0800

    user逻辑的完善

--- common/model/pojo/role.go
+++ common/model/pojo/role.go
@@ -6,17 +6,21 @@ import (
 )
 
 type User struct {
-	Username    string    `form:"username" json:"username" gorm:"column:username"`
-	Password    string    `form:"password" json:"password" gorm:"column:password"`
-	ID          int64     `json:"id" gorm:"primaryKey;column:id" form:"id"`
-	Email       string    `form:"email" json:"email" gorm:"column:email"`
-	Telephone   string    `form:"telephone" json:"telephone" gorm:"column:telephone"`
-	UUID        uuid.UUID `json:"uuid" gorm:"column:uuid"`
-	Status      int64     `json:"status" gorm:"column:status" form:"status"` // 用户是否被冻结
-	HeadImg     string    `json:"headImg" form:"head-img"`
-	AuthorityId uint      `json:"authorityId" gorm:"default:888;comment:用户角色ID"` // 角色ID
-	Authority   string    `json:"authority" gorm:"default:user;comment:用户角色"`
-	GetLike     int       `json:"get_like" gorm:"default:0;comment:收到的点赞数"`
+	Username        string    `form:"username" json:"username" gorm:"column:username"`
+	Password        string    `form:"password" json:"password" gorm:"column:password"`
+	ID              int64     `json:"id" gorm:"primaryKey;column:id" form:"id"`
+	Email           string    `form:"email" json:"email" gorm:"column:email"`
+	Telephone       string    `form:"telephone" json:"telephone" gorm:"column:telephone"`
+	UUID            uuid.UUID `json:"uuid" gorm:"column:uuid"`
+	Status          int64     `json:"status" gorm:"column:status" form:"status"` // 用户是否被冻结
+	HeadImg         string    `json:"headImg" form:"head-img"`
+	AuthorityId     uint      `json:"authorityId" gorm:"default:888;comment:用户角色ID"` // 角色ID
+	Authority       string    `json:"authority" gorm:"default:user;comment:用户角色"`
+	BackgroundImage string    `json:"background_image"` // 个人主页背景图
+	Signature       string    `json:"signature"`        // 个人简介
+	TotalFavorited  int64     `json:"total_favorited"`  // 获赞数量
+	WorkCount       int64     `json:"work_count"`       // 作品数量
+	FavoriteCount   int64     `json:"favorite_count"`   // 点赞数量
 }
 
 type Admin struct {
