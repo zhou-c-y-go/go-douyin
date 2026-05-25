694684d6c3faf4266dca6b457e3a6e6037f089c2
zhou-c-y-go <2553598443@qq.com>
2026-05-25 20:29:33 +0800

    添加返回给前端的专属与个人空间信息的数据结构

--- common/model/response/reponse.go
+++ common/model/response/reponse.go
@@ -33,3 +33,15 @@ func Fail(c *gin.Context, _ BusinessCode, msg string) {
 		msg,
 	})
 }
+
+// UserProfileResponse 定义返回给前端的专属数据结构，做好字段名映射
+type UserProfileResponse struct {
+	ID              int64  `json:"id"`
+	Username        string `json:"username"`
+	Avatar          string `json:"avatar"` // 对应后端的 HeadImg
+	BackgroundImage string `json:"background_image"`
+	Signature       string `json:"signature"`
+	TotalLiked      int64  `json:"total_liked"` // 对应前端的 total_liked (后端 TotalFavorited)
+	WorkCount       int64  `json:"work_count"`
+	FavoriteCount   int64  `json:"favorite_count"`
+}
