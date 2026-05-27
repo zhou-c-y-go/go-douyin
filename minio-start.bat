579c2fcc72e3b8e46ed0428340bfac429213610b
zhou-c-y-go <2553598443@qq.com>
2026-05-27 16:40:16 +0800

    chore: 解除 minio-start.bat 的 Git 追踪

--- minio-start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-cd /d "D:\minio\bin"
-
-echo 🚀 MinIO Object Storage Service is starting...
-echo 🔗 Web Console: http://127.0.0.1:9000
-echo ⚡ API Endpoint: http://127.0.0.1:9005
-minio.exe server D:\minio\data --console-address "127.0.0.1:9005" --address "127.0.0.1:9000"
-pause
