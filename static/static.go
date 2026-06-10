48c21eb738e57522e560f3975e303121056ca1d7
zhou-c-y-go <2553598443@qq.com>
2026-06-10 20:42:15 +0800

    字段拼接

--- static/static.go
+++ static/static.go
@@ -3,3 +3,20 @@ package static
 import "time"
 
 const Jwt_time = 7 * 24 * time.Hour
+
+var likeList = []string{"video", "article", "imageText"}
+var favoriteList = []string{"video", "article", "imageText"}
+
+func GetLikeList() []string {
+	// 拷贝一份返回，隔离读写
+	res := make([]string, len(likeList))
+	copy(res, likeList)
+	return res
+}
+
+func GetFavoriteList() []string {
+	// 拷贝一份返回，隔离读写
+	res := make([]string, len(favoriteList))
+	copy(res, favoriteList)
+	return res
+}
